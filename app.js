5be1fb3bba106e3b94fd3c90512633807597ee98
jinhyeokfang <jinhyeokfang@gmail.com>
2018-09-28 19:51:02 +0900

    add update function

--- app.js
+++ app.js
@@ -9,25 +9,34 @@ let room = {
     users: []
 }
 
+io.set('origins', '*:*');
 io.on('connection', socket => {
-    newUser(socket.id)
+    addUser(socket.id)
     socket.on("update", data => {
-
+        updateUser(socket.id, data)
     })
     socket.on("disconnect", data => {
         removeUser(socket.id)
     })
+
+    io.emit("update", room)
 })
 
+function addUser(id) {
+    room.users.push({x: 0, y: 0, id: id, rotation: 0, tail: []})
+}
+
+function updateUser(id, data) {
+    let userIndex = getUserIndex(id)
+    room.users[userIndex] = data
+}
+
 function removeUser(id) {
-    let userIndex = room.users.findIndex(element => element.id == id)
+    let userIndex = getUserIndex(id)
     room.users.splice(userIndex, 1)
     io.emit("update", room)
 }
 
-function moveUser(id, x, y) {
-    let user = room.users.find(element => element.id == id)
-    user.x += x
-    user.y += y
-    io.emit("update", room)
+function getUserIndex(id) {
+    return room.users.findIndex(element => element.id == id)
 }
