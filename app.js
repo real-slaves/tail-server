21ff035a54b2cca065d7171e48605d64c185a6a3
jinhyeokfang <jinhyeokfang@gmail.com>
2018-09-30 16:32:39 +0900

    add waiting

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
-app.use(express.static('/game'))
+app.use(express.static('./game'))
 
 server.listen(process.env.PORT || 8080)
 
-let room = {
-    users: []
+let game = {
+	users: [],
+	room: {
+		status: 0
+	}
 }
 
-io.set('origins', '*:*');
+io.set('origins', '*:*')
 io.on('connection', socket => {
     addUser(socket.id)
     socket.on("update", data => {
@@ -19,29 +22,47 @@ io.on('connection', socket => {
     })
     socket.on("disconnect", data => {
         removeUser(socket.id)
+	clearInterval(sendData)
     })
-
-    setInterval(() => {
-        io.emit("update", room)
-    }, 50)
+    
+    let sendData = setInterval(() => {
+	console.log(game)
+        socket.emit("update", { 
+		users: game.users.filter(element => element.roomid == game.users[getUserIndex(socket.id)].roomid), 
+		room: game.room
+	})
+    }, 100)
 })
 
 function addUser(id) {
-    room.users.push({x: 0, y: 0, id: id, rotation: 0, tail: []})
+    if (getRoomSNumberOfUser(0) < 4 && game.room.status == 0) {
+	game.users.push({x: 0, y: 0, id: id, rotation: 0, tail: [], roomid: 0})
+	if (getRoomSNumberOfUser(0) == 4)
+	    startGame(0)
+    } else {
+	game.users.push({x: 0, y: 0, id: id, rotation: 0, tail: [], roomid: -1})
+    }
 }
 
 function updateUser(id, data) {
     let userIndex = getUserIndex(id)
-    room.users[userIndex] = data
-    room.users[userIndex].id = id
+    game.users[userIndex] = data
+    game.users[userIndex].id = id
 }
 
 function removeUser(id) {
     let userIndex = getUserIndex(id)
-    room.users.splice(userIndex, 1)
-    io.emit("update", room)
+    game.users.splice(userIndex, 1)
+}
+
+function startGame(roomid) {
+    game.room.status == 1
+}
+
+function getRoomSNumberOfUser(roomid) {
+    return game.users.filter(element => element.roomid == roomid).length
 }
 
 function getUserIndex(id) {
-    return room.users.findIndex(element => element.id == id)
+    return game.users.findIndex(element => element.id == id)
 }
