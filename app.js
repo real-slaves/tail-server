4e562653a5607527e2d8c1db618804b20ccebf24
jinhyeokfang <jinhyeokfang@gmail.com>
2018-10-06 09:06:49 +0000

    create new room automatically

--- app.js
+++ app.js
@@ -28,34 +28,39 @@ io.on('connection', socket => {
 	userDied(data.hunter, data.target)
     })
     socket.on("disconnect", data => {
-	let user = game.users.find(element => element.id == socket.id)
+	let user = getUser(socket.id)
 	if (user == undefined)
 		return;
-	if (user.roomid >= 0 && game.rooms[0].status == 1 && user.isDead == undefined)
+	if (user.roomid >= 0 && game.rooms[user.roomid].status == 1 && user.isDead == undefined)
 		userDied(null, user.id)
 	removeUser(socket.id)
     })
 })
 
 setInterval(() => {
-	console.log(game)
-	if (isGameOver(0))
-		userWon(game.users.find(element => element.roomid == 0 && element.isDead == false))
-
-	game.users.forEach(element => io.to(element.id).emit("update", {
-		users: getRoomSUserList(game.users[getUserIndex(element.id)].roomid),
-		room: game.rooms[0]
-	}))
+    monitoring()
+    game.users.forEach(element => io.to(element.id).emit("update", {
+	users: getRoomSUserList(getUser(element.id).roomid),
+	room: game.rooms[element.roomid]
+    }))
+	
+    game.rooms.forEach((element, index) => {
+	if (isGameOver(index))
+		userWon(game.users.find(element => element.roomid == index && element.isDead == false))
+    })
 }, 100)
+
+function monitoring() {
+    console.log(game)
+    console.log(game.rooms[0].foodchain)
+}
 
 function addUser(id) {
     let numberOfUsers = 4
-    if (getRoomSNumberOfUser(0) < numberOfUsers && game.rooms[0].status == 0) {
-	game.users.push({x: 0, y: 0, id: id, rotation: 0, tail: [], roomid: 0, isDead: false})
-	if (getRoomSNumberOfUser(0) == numberOfUsers)
-	    startGame(0)
-    } else {
-	game.users.push({x: 0, y: 0, id: id, rotation: 0, tail: [], roomid: -1, isDead: false})
+    game.users.push({x: 0, y: 0, id: id, rotation: 0, tail: [], roomid: game.rooms.length - 1, isDead: false})
+    if (getRoomSNumberOfUser(game.rooms.length - 1) == numberOfUsers) {
+        startGame(game.rooms.length - 1)
+        createNewRoom()
     }
 }
 
@@ -67,43 +72,42 @@ function updateUser(id, data) {
 
 function userDied(hunter, target) {
     io.to(target).emit("died")
-    if (hunter == null)
+    if (!hunter)
 	return;
 
-    if (game.rooms[0].foodchain.find(element => element.hunter == target) && game.rooms[0].foodchain.find(element => element.target == target))
+    if (game.rooms[getUser(hunter).roomid].foodchain.find(element => element.hunter == target) && game.rooms[getUser(hunter).roomid].foodchain.find(element => element.target == target))
 	console.log("")	
     else
 	return;
 
-    game.rooms[0].foodchain.splice(game.rooms[0].foodchain.findIndex(element => element.target == target), 1)
-    game.rooms[0].foodchain[game.rooms[0].foodchain.findIndex(element => element.hunter == target)].hunter = hunter   
-    game.users[game.users.findIndex(element => element.id == target)].isDead = true
+    game.rooms[getUser(hunter).roomid].foodchain.splice(game.rooms[getUser(hunter).roomid].foodchain.findIndex(element => element.target == target), 1)
+    game.rooms[getUser(hunter).roomid].foodchain[game.rooms[getUser(hunter).roomid].foodchain.findIndex(element => element.hunter == target)].hunter = hunter   
+    game.users[getUserIndex(target)].isDead = true
 }
 
 function userWon(winner) {
+    removeRoom(winner.roomid)
     getRoomSUserList(winner.id).forEach(element => {
 	game.users[game.users.findIndex(element => element.roomid != -2)].roomid = -2
 	game.users[game.users.findIndex(element => element.isDead != false)].isDead = false
 	io.to(element.id).emit("gameEnd", {winner: winner} )
     })
     game.users.filter(element => element.roomid == winner.roomid).forEach(element => game.users[getUserIndex(element.id)].roomid = -2) 
-    game.rooms[0].status = 0
 }
 
 function removeUser(id) {
-    let userIndex = getUserIndex(id)
-    if (getRoomSNumberOfUser(-1) != 0 && game.users[userIndex].roomid != -1 && game.rooms[0].status == 0)
+    if (getRoomSNumberOfUser(-1) != 0 && getUser(id).roomid != -1 && game.rooms[0].status == 0)
 	game.users[game.users.findIndex(element => element.roomid == -1)].roomid = 0
 	
-    game.users.splice(userIndex, 1)
+    game.users.splice(getUserIndex(id), 1)
 }
 
-function createNewRoom(roomid) {
-	game.rooms.push({status: 0, blocks: [], foodchain: [], roomid: roomid})
+function createNewRoom() {
+	game.rooms.push({status: 0, blocks: [], foodchain: []})
 }
 
 function removeRoom(roomid) {
-	game.rooms.splice(getRoomIndex(roomid), 1)
+	game.rooms[roomid] = {status: 0, blocks: [], foodchain: []}
 }
 
 function getRoom(roomid) {
@@ -126,12 +130,12 @@ function putBlock(roomid) {
 
 function startGame(roomid) {
     game.rooms[roomid].status = 1
-    game.rooms[roomid].foodchain = makeFoodchain(game.users)
+    game.rooms[roomid].foodchain = makeFoodchain(game.users, roomid)
 }
 
-function makeFoodchain(users) {
+function makeFoodchain(users, roomid) {
     let foodchain = []
-    let userlist = getRoomSUserList(0)
+    let userlist = getRoomSUserList(roomid)
     for (let i=0; i<userlist.length; i++) 
 	foodchain.push({hunter: userlist[i].id, target: userlist[i != userlist.length - 1 ? i+1 : 0].id})
      
@@ -148,6 +152,10 @@ function getRoomSNumberOfUser(roomid) {
 
 function getUserIndex(id) {
     return game.users.findIndex(element => element.id == id)
+}
+
+function getUser(id) {
+    return game.users.find(element => element.id == id)
 }
 
 function isGameOver(roomid) {
